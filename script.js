718bf5bab721715381df3be6c17a13c48f615c6a
hsum4 <93267846+hsum4@users.noreply.github.com>
2025-02-05 15:38:03 +0800

    add function to add library to table

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const libraryTable = document.getElementById("library-table");
 
 
 function Book(title, author, pages, read) {
@@ -13,5 +14,22 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
+function addLibraryToTable(myLibrary) {
+    libraryTable.innerHTML = "";
+    myLibrary.forEach(element => {
+        const tr = document.createElement("tr");
+        for (const data in element) {
+            if (element.hasOwnProperty(data)) {
+                const td = document.createElement('td');
+                td.textContent = element[data];
+                tr.appendChild(td);
+            }
+        }
+        libraryTable.appendChild(tr);
+    });
+}
+
 addBookToLibrary("hobbt", "jjtolkin", 203, "no");
 addBookToLibrary("harry poter", "idunno", 233, "yea");
+
+addLibraryToTable(myLibrary);
