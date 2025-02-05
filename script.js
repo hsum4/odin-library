df70dd727456b12149879a6c94631a4d2d375819
hsum4 <93267846+hsum4@users.noreply.github.com>
2025-02-05 14:09:38 +0800

    finish addBookToLibrary function

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ this.pages = pages
 this.read = read;
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
+
+addBookToLibrary("hobbt", "jjtolkin", 203, "no");
+addBookToLibrary("harry poter", "idunno", 233, "yea");
